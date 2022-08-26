7d9e32c7b72aef21797bb0814e2f906bb7202474
NLxDoDge <nlxdodge@gmail.com>
2022-08-26 20:28:10 +0200

    Fix prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-recommended'
+    'plugin:vue/vue3-recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
